c16baa421f56c52c265b46009145efc68c767a48
Johnny Shuford <airforce@Johnnys-MacBook-Air.local>
2025-06-04 19:21:06 -0600

    creating the guest data model

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Guest
 
-# Register your models here.
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'attending', 'group_id')
+    fields = (
+        'first_name',
+        'last_name',
+        'email',
+        'attending',
+        'group_id',  # 👈 add this to make it editable!
+        'dietary_restrictions',
+        'message_for_couple',
+    )
--- rsvp/models.py
+++ rsvp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Guest(models.Model):
+    group_id = models.UUIDField(default=uuid.uuid4, editable=True, db_index=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    dietary_restrictions = models.TextField(blank=True)
+    message_for_couple = models.TextField(blank=True)
+    attending = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
